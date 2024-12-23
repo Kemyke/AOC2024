8c50ccfa38ad0e506ea1f1c9cc523629420105fb
Attila Kemeny <kemyyy@gmail.com>
2024-12-23 22:58:02 +0100

    Day 23

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,15 +1,70 @@
-﻿NumPad np = new NumPad();
+﻿long GetPartLength(int iter, string part, int maxIter, DirPad dp, Dictionary<int, Dictionary<string, long>> cache)
+{
+    if (!cache.ContainsKey(iter))
+        cache.Add(iter, new Dictionary<string, long>());
+
+    if (!cache.ContainsKey(iter+1))
+        cache.Add(iter+1, new Dictionary<string, long>());
+
+
+    if (cache[iter].ContainsKey(part))
+        return cache[iter][part];
+
+    if (iter == maxIter)
+    {
+        cache[iter].Add(part, part.Length);
+        return part.Length;
+    }
+
+    var trets = new List<string> { "" };
+    for (int i = 0; i < part.Length; i++)
+    {
+        var s = dp.FromTo(i < 1 ? DirPad.nums['A'] : DirPad.nums[part[i - 1]], DirPad.nums[part[i]]);
+        trets = trets.SelectMany(fl => s.Select(sl => fl + sl + "A")).Distinct().ToList();
+    }
+
+    var mintl = trets.Min(t => t.Length);
+    var ts = trets.Where(t => t.Length == mintl);
+    var newParts = ts.SelectMany(t => t.Split("A").Select(s => s + "A")).Distinct().ToList();
+    if(iter==0)
+    {
+        if (ts.Select(t => t.Split("A").Select(s => s + "A")).Any(s => s.Any(ss => ss == "<A")))
+        {
+
+        }
+    }
+    foreach(var np in newParts)
+    {
+        if (!cache[iter + 1].ContainsKey(np))
+        {
+            var npr = GetPartLength(iter + 1, np, maxIter, dp, cache);
+        }
+    }
+    
+    var tss = ts.Select(t => t.Split("A").Select(s => s + "A")).Select(t => t.Sum(tt => cache[iter + 1][tt]));
+    var ret = tss.Min(t => t) - 1;
+    cache[iter].Add(part, ret);
+    if (iter == 1)
+    {
+       
+    }
+    return ret;
+}
+
+
+NumPad np = new NumPad();
 DirPad dp = new DirPad();
 
 var ret1 = 0;
-
+long ret2 = 0;
 var input = File.ReadAllLines("input.txt").ToList();
-var cache = new Dictionary<int, Dictionary<string, List<string>>>();
+var cache = new Dictionary<int, Dictionary<string, long>>();
+var maxIter = 25;
 
 foreach (var line in input)
 {
     var cs = np.CodeCoordinates(line);
-    
+
     List<string> numPadSeq = new List<string> { "" };
     for (int i = 0; i < cs.Count; i++)
     {
@@ -23,39 +78,124 @@ foreach (var line in input)
         numPadSeq = rets;
     }
 
-    List<string> dirSeq = numPadSeq.ToList();
-    List<string> retSeq = new List<string>();
-    for(int rs = 0; rs < 2; rs++)
+    long pr22 = long.MaxValue;
+    foreach(var nps in numPadSeq)
     {
-        if (!cache.ContainsKey(rs))
-            cache.Add(rs, new Dictionary<string, List<string>>());
-
-        retSeq.Clear();
-        foreach (var rr in dirSeq)
+        var ps = nps.Split("A").Select(s => s + "A").ToList();
+        long pr2 = 0;
+        foreach(var p in ps)
         {
-            var rrparts = rr.Split("A").Select(s=>s+"A").ToList();
-            List<string> trets = new List<string> { "" };
-
-            foreach (var part in rrparts)
-            {
-                for (int i = 0; i < part.Length; i++)
-                {
-                    var s = dp.FromTo(i < 1 ? DirPad.nums['A'] : DirPad.nums[part[i - 1]], DirPad.nums[part[i]]);
-                    trets = trets.SelectMany(fl => s.Select(sl => fl + sl + "A")).Distinct().ToList();
-                }
-            }
-
-            retSeq.AddRange(trets.Select(t=>t.Substring(0, t.Length - 1)));
+            pr2 += GetPartLength(0, p, maxIter, dp, cache);
         }
-        dirSeq = retSeq.ToList();
+        
+        if (pr22 > pr2 - 1)
+            pr22 = pr2 - 1;
     }
-
-    var x = retSeq.Select(r => r.Length).OrderBy(v => v).First();
     var num = int.Parse(line.Substring(0, line.Length - 1));
-    ret1 += x * num;
-    Console.WriteLine($"{x} * {num}");
+    Console.WriteLine($"{pr22} * {num}");
+    ret2 += num * pr22;
+
 }
-Console.WriteLine(ret1);
+
+//foreach (var line in input)
+//{
+//    var cs = np.CodeCoordinates(line);
+
+//    List<string> numPadSeq = new List<string> { "" };
+//    for (int i = 0; i < cs.Count; i++)
+//    {
+//        var s = np.FromTo(i < 1 ? NumPad.nums['A'] : cs[i - 1], cs[i]);
+//        List<string> rets = new List<string>();
+//        foreach (var r in numPadSeq)
+//            foreach (var ss in s)
+//            {
+//                rets.Add(r + ss + "A");
+//            }
+//        numPadSeq = rets;
+//    }
+
+//    List<string> dirSeq = numPadSeq.ToList();
+//    List<string> retSeq = new List<string>();
+//    List<string> currList = null;
+//    foreach (var dsr in dirSeq)
+//    {
+//        for(int rs = 0; rs < maxIter; rs++)
+//        {
+//            if (!cache.ContainsKey(rs))
+//                cache.Add(rs, new Dictionary<string, List<string>>());
+
+//            if (rs == 0)
+//                currList = new List<string> { dsr };
+
+
+//            var tempList = new List<string>();
+//            foreach (var rr in currList)
+//            {
+//                if (!cache[rs].ContainsKey(rr))
+//                {
+//                    var rrparts = rr.Split("A").Select(s => s + "A").ToList();
+//                    List<string> trets = new List<string> { "" };
+//                    foreach (var part in rrparts)
+//                    {
+//                        for (int i = 0; i < part.Length; i++)
+//                        {
+//                            var s = dp.FromTo(i < 1 ? DirPad.nums['A'] : DirPad.nums[part[i - 1]], DirPad.nums[part[i]]);
+//                            trets = trets.SelectMany(fl => s.Select(sl => fl + sl + "A")).Distinct().ToList();
+//                        }
+//                    }
+//                    var ttl = trets.Select(t => t.Substring(0, t.Length - 1)).ToList();
+//                    cache[rs][rr] = ttl;
+//                    tempList.AddRange(ttl);
+//                }
+//                else
+//                {
+//                    tempList.AddRange(cache[rs][rr]);
+//                }
+//            }
+//            currList = tempList;
+//            if (rs == maxIter - 1)
+//                retSeq.AddRange(currList);
+//        }
+//    }
+
+//    var num = int.Parse(line.Substring(0, line.Length - 1));
+//    var x = retSeq.OrderBy(c => c.Length).First().Length;
+//    Console.WriteLine($"{x} * {num}");
+
+//    ret2 += x * num;
+
+
+//for (int rs = 0; rs < 2; rs++)
+//{
+//    if (!cache.ContainsKey(rs))
+//        cache.Add(rs, new Dictionary<string, List<string>>());
+
+//    retSeq.Clear();
+//    foreach (var rr in dirSeq)
+//    {
+//        var rrparts = rr.Split("A").Select(s=>s+"A").ToList();
+//        List<string> trets = new List<string> { "" };
+
+//        foreach (var part in rrparts)
+//        {
+//            for (int i = 0; i < part.Length; i++)
+//            {
+//                var s = dp.FromTo(i < 1 ? DirPad.nums['A'] : DirPad.nums[part[i - 1]], DirPad.nums[part[i]]);
+//                trets = trets.SelectMany(fl => s.Select(sl => fl + sl + "A")).Distinct().ToList();
+//            }
+//        }
+
+//        retSeq.AddRange(trets.Select(t=>t.Substring(0, t.Length - 1)));
+//    }
+//    dirSeq = retSeq.ToList();
+//}
+
+//var x = retSeq.Select(r => r.Length).OrderBy(v => v).First();
+//var num = int.Parse(line.Substring(0, line.Length - 1));
+//ret1 += x * num;
+//Console.WriteLine($"{x} * {num}");
+//}
+Console.WriteLine(ret2);
 Console.ReadLine();
 
 class Coordinate
---
+++ Day23/Program.cs
@@ -0,0 +1,63 @@
+﻿var input = File.ReadAllLines("input.txt").ToList();
+Dictionary<string, Computer> comps = new Dictionary<string, Computer>();
+
+foreach(var line in input)
+{
+    var ss = line.Split("-");
+    if (!comps.ContainsKey(ss[0]))
+        comps.Add(ss[0], new Computer { Name = ss[0] });
+    if (!comps.ContainsKey(ss[1]))
+        comps.Add(ss[1], new Computer { Name = ss[1] });
+
+    comps[ss[0]].Connection.Add(ss[1]);
+    comps[ss[1]].Connection.Add(ss[0]);
+}
+
+HashSet<string> triplets = new HashSet<string>();
+
+foreach(var cn in comps.Keys)
+{
+    var comp = comps[cn];
+
+    foreach(var c1 in comp.Connection)
+    {
+        foreach(var c2 in comp.Connection)
+        {
+            if (c1 != c2 && comps[c1].Connection.Contains(c2))
+            {
+                triplets.Add(string.Join("", new List<string> { cn, c1, c2 }.OrderBy(c => c)));
+            }
+        }
+    }
+}
+
+var ret1 = triplets.Count(t => t[0] == 't' || t[2] == 't' || t[4] == 't');
+string rr2 = "";
+
+while (triplets.Any())
+{
+    var ol = triplets.ToList();
+    triplets.Clear();
+    foreach (var t in ol)
+    {
+        var ncs = t.Chunk(2).Select(c => new string(c)).ToHashSet();
+        foreach (var c in comps)
+        {
+            if(!ncs.Contains(c.Key) && ncs.All(nc => comps[nc].Connection.Contains(c.Key)))
+            {
+                triplets.Add(string.Join("", ncs.Append(c.Key).OrderBy(c => c)));
+            }
+        }
+    }
+    rr2 = ol.First();
+}
+
+Console.WriteLine(ret1);
+Console.WriteLine(string.Join(",",rr2.Chunk(2).Select(c => new string(c))));
+Console.ReadLine();
+
+class Computer
+{
+    public string Name { get; set; }
+    public HashSet<string> Connection { get; set; } = new HashSet<string>();
+}
